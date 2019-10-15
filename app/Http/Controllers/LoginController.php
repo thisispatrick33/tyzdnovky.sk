46bf6399820a42ec7860bc94c84531a623e5c70f
Jakub <jakubednar@gmail.com>
2019-10-15 09:01:11 +0200

    response changes in logincontroller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -24,7 +24,7 @@ class LoginController extends Controller
             return response()->json([
                 'success' => false,
                 'data' => [],
-                'messages' => $validator->messages()
+                'messages' => $validator->messages()->all()
             ]);           
         };
 
