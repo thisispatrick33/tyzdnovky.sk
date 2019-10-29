2687fc9f32eb1604ab00a6441d5abb84501d492c
Jakub <jakubednar@gmail.com>
2019-10-29 18:02:17 +0100

    password reset,messages,email view,env change

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -12,6 +12,7 @@ use App\Branch;
 use App\Company;
 use App\User;
 use App\Language;
+use Mail;
 
 class LoginController extends Controller
 {
@@ -58,7 +59,6 @@ class LoginController extends Controller
                                       'name' => $user->name, 
                                       'lastName' => $user->lastname,
                                       'phone' => $user->phone,
-                                      'ready' => $user->nastup,
                                       'email'=> $user->email,
                                       'active' => $user->active,
                                       'drivingLicense' => $user->driving_license,
@@ -88,7 +88,6 @@ class LoginController extends Controller
                                     'name' => $company->name, 
                                     'bussinesId' => $company->ico,
                                     'phone' => $company->phone,
-                                    'ready' => $company->nastup,
                                     'email'=> $company->email,
                                     'active' => $company->active,
                                     'username' => $company->username
@@ -102,6 +101,170 @@ class LoginController extends Controller
                              'messages' => trans('auth.failed')
                             ];
             }  
+            return response()->json($response);
+        }
+        /*else if(){
+            TODO admin verification
+        }*/
+        else{
+            return response()->json([
+                'success' => false,
+                'data' => [],
+                'messages' => trans('messages.userNotFound')
+            ]);
+        }
+    }
+
+    public function passResetEmail(Request $request){
+        $validator = Validator::make($request->all(), [
+            'login' => 'required',
+        ]);   
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'data' => [],
+                'messages' => $validator->messages()->all()
+            ]);           
+        };
+
+        $user = false;
+        $company = false;
+
+        if($findUser = User::where('email',$request->login)->first()){
+            $user = $findUser;
+        }
+        else if($findUser = User::where('username',$request->login)->first()){
+            $user = $findUser;
+        }
+        else if($findCompany = Company::where('username',$request->login)->first()){
+            $company = $findCompany;
+        }
+        else if($findCompany = Company::where('email',$request->login)->first()) {
+            $company = $findCompany;
+        }
+
+        if($user){
+            $data=$user;
+        }
+        else if($company){
+            $data=$company;
+        }
+        /*else if(){
+            TODO admin verification
+        }*/
+        else{
+            return response()->json([
+                'success' => false,
+                'data' => [],
+                'messages' => trans('messages.userNotFound')
+            ]);
+        }
+
+        $link = url("/reset-password?token=".$data->auth_token );
+        $name = $data->active?$data->username:$data->email;
+        Mail::send("email.passReset", ['action_url'=>$link,'name'=>$name,'time'=>config('jwt.ttl')], function($message) use($data) {
+            $message->to($data->email);
+            $message->subject('Tyzdnovky.sk | Password reset');
+            $message->from('tyzdnovky@azet.sk');
+        });
+        if(count(Mail::failures()) > 0){
+            return response()->json([
+                'success' => false,
+                'data' => [],
+                'messages' => trans('messages.emailFailed')
+            ]); 
+        }
+        else{
+            return response()->json([
+                'success' => true,
+                'data' => [],
+                'messages' => trans('messages.emailSent')
+            ]); 
+        }
+    }
+
+    public function passResetDo(Request $request){
+        $validator = Validator::make($request->all(), [
+            'token' => 'required',
+            'password' => 'required'
+        ]);   
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'data' => [],
+                'messages' => $validator->messages()->all()
+            ]);           
+        };
+
+        $user = false;
+        $company = false;
+
+        if($findUser = User::where('auth_token',$request->token)->first()){
+            $user = $findUser;
+        }
+        else if($findCompany = Company::where('auth_token',$request->token)->first()) {
+            $company = $findCompany;
+        }
+
+        if($user){
+            $token = \Auth::guard('users')->fromUser($user);
+            $user->auth_token = $token;
+            $user->password = \Hash::make($request->password);
+            $response;
+            if($user->save()){
+                $response = [
+                    'success' => true,
+                    'data' => [ 'type' => 'user',
+                                'id' => $user->id,
+                                'auth_token' => $user->auth_token,
+                                'name' => $user->name, 
+                                'lastName' => $user->lastname,
+                                'phone' => $user->phone,
+                                'email'=> $user->email,
+                                'active' => $user->active,
+                                'drivingLicense' => $user->driving_license,
+                                'username' => $user->username
+                             ],
+                    'messages' => trans('messages.passwordChanged')
+                ];
+            }
+            else{
+                $response = [
+                    'success' => false,
+                    'data' => [],
+                    'messages' => trans('messages.passwordFail')
+                ];
+            }
+            return response()->json($response);
+        }
+        else if($company){
+            $token = \Auth::guard('companies')->fromUser($company);
+            $company->auth_token = $token;
+            $company->password = \Hash::make($request->password);
+
+            if($company->save()){
+                $response = [
+                    'success' => true,
+                    'data' => [ 'type' => 'company',
+                                'id' => $company->id,
+                                'auth_token' => $company->auth_token,
+                                'name' => $company->name, 
+                                'bussinesId' => $company->ico,
+                                'phone' => $company->phone,
+                                'email'=> $company->email,
+                                'active' => $company->active,
+                                'username' => $company->username
+                            ],
+                    'messages' => trans('messages.passwordChanged')
+                ];
+            }
+            else{
+                $response = [
+                    'success' => false,
+                    'data' => [],
+                    'messages' => trans('messages.passwordFail')
+                ];
+            }
             return response()->json($response);
         }
         /*else if(){
