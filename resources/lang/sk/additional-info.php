96e4323e9f9291d5216a19cbb6b4a004cb06e32f
Jakub <jakubednar@gmail.com>
2019-11-18 17:03:58 +0100

    texty additional info

---
+++ resources/lang/sk/additional-info.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Additional info text
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'slide1_title1' => 'Nastavte svoj profil ...',
+    'slide1_subtitle1' => 'Kto ste ?',
+    'slide1_placeholder1' => 'Napíšte svoje meno',
+    'slide1_placeholder2' => 'Napíšte svoje priezvisko',
+    'slide1_subtitle2' => 'Kontaktujte ma tu.',
+    'slide1_placeholder3' => 'Napíšte svoje tel. číslo',
+    'slide2_title1' => 'Jazykové zručnosti .',
+    'slide2_placeholder1' => 'Ovládate ďalši jazyk ?',
+    'slide3_title1' => 'Práce na plný úväzok',
+    'slide3_subtitle1' => 'z akého odvetvia chcete dostávať pracovné ponuky ?',
+    'slide4_title1' => 'Voľno časový úväzok',
+    'slide4_subtitle1' => 'čo by ste chceli robiť ?',
+    'slide5_title1' => 'Nastavte svoj profil ...',
+    'slide5_placeholder1' => 'Napíšte svoje meno',
+    'slide5_checkbox1' => 'Mám vodičský preukaz skupiny B',
+    'slide5_submit' => 'Uložiť zmeny',    
+];
