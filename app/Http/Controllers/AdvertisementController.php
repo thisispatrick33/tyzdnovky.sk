e5efb07b22426c581df3dd95fd36510315fd2b0b
Jakub <jakubednar@gmail.com>
2019-11-25 20:33:39 +0100

    Advertisement get advertisements v3

--- app/Http/Controllers/AdvertisementController.php
+++ app/Http/Controllers/AdvertisementController.php
@@ -221,7 +221,10 @@ class AdvertisementController extends Controller
             }
     
             foreach($ad->branches as $branch){
-                array_push($branches,$branch->id);
+                $branchNew['id']=$branch->id;
+                $branchNew['free_time']=$branch->free_time;
+                $branchNew['name']=$branch->name;
+                array_push($branches,$branchNew);
             }
 
             $adNew = [
@@ -255,7 +258,10 @@ class AdvertisementController extends Controller
         }
 
         foreach($ad->branches as $branch){
-            array_push($branches,$branch->id);
+            $branchNew['id']=$branch->id;
+            $branchNew['free_time']=$branch->free_time;
+            $branchNew['name']=$branch->name;
+            array_push($branches,$branchNew);
         }
 
         $res = [
